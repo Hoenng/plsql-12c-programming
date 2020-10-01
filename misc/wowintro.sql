69c0ed79ab20d6b21ea3106780b83b0aef5e40de
Hoenng <deandouglas@protonmail.com>
2020-10-01 12:18:42 +0100

    Added misc folder

---
+++ misc/wowintro.sql
@@ -0,0 +1,9 @@
+-- wowintro.sql
+
+create or replace 
+function WOWINTRO
+RETURN VARCHAR2 IS
+BEGIN
+RETURN 'No one would have believed in the last years of the nineteenth century CHR(10)
+That human affairs were being watched from the timeless worlds of space';
+end;
